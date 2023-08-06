5a359143792246c60583c953540be67a700b5575
Bozhidar Bozhilov <bozhidarbozhilov@gmail.com>
2023-08-06 23:24:22 +0300

    Add Shell and Merge sort examples

---
+++ src/MergeSort.java
@@ -0,0 +1,39 @@
+public class MergeSort {
+    public static int[] sort(int[] array){
+        mergeSort(array, 0, array.length);
+
+        return array;
+
+    }
+
+    private static void mergeSort(int[] input, int start, int end){
+        if(end - start < 2){
+            return;
+        }
+
+        int mid = (start + end)/2;
+
+        mergeSort(input, start, mid);
+        mergeSort(input, mid, end);
+        merge(input, start, mid, end);
+    }
+
+    private static void merge(int[] input, int start, int mid, int end){
+
+        if(input[mid-1] <= input[mid]){
+            return;
+        }
+        int i = start;
+        int j = mid;
+        int tempIndex = 0;
+        int[] temp = new int[end - start];
+
+        while(i<mid && j<end){
+            temp[tempIndex++] = input[i] <= input[j] ? input[i++] : input[j++];
+        }
+
+        System.arraycopy(input, i, input, start + tempIndex, mid - i);
+        System.arraycopy(temp, 0, input, start, tempIndex);
+    }
+
+}
---
+++ src/ShellSort.java
@@ -0,0 +1,17 @@
+public class ShellSort {
+    public static int[] sort(int[] array){
+        
+        for(int gap = array.length/2; gap>0;gap/=2){
+            for (int i = gap; i < array.length ; i++) {
+                int newElement = array[i];
+                int j = i;
+                while(j-gap>=0 && array[j-gap] > newElement){
+                    array[j] = array[j-gap];
+                    j-=gap;
+                }
+                array[j] = newElement;
+            }
+        }
+        return array;
+    }
+}
