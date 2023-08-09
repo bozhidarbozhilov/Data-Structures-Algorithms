ddd65d73ce617986158fa94c3311e85f41aa1277
Bozhidar Bozhilov <bozhidarbozhilov@gmail.com>
2023-08-09 05:42:58 +0300

    Add Counting sort example

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        int[] intArray = { 20, 35, -15, 7, 55, 1, -22 };
+        //int[] intArray = { 20, 35, -15, 7, 55, 1, -22 };
+        int[] intArray = { 2, 5, 9, 8, 2, 8, 7, 10, 4, 3 };
 
-        int[] sorted = QuickSort.sort(intArray);
+        int[] sorted = CountingSort.sort(intArray);
 
         for(int element: sorted){
             System.out.println(element);
